ce7245d7bcdee0d613822e0970cba77764ee6345
nareshpenchikala <penchikalanaresh62@gmail.com>
2021-08-16 11:27:10 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "access" {
+  description = "access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret" {
+  description = "secret key"
+  type        = string
+  sensitive   = true
+}
